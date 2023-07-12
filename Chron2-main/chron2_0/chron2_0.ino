76089448ca97781c03f4f17555cc6d2fb474a096
Nikolay Gubbe <n.gubbe@limecreditgroup.com>
2023-07-13 02:53:26 +0700

    добавлен вывод сервисной информации с датчиков

--- Chron2-main/chron2_0/chron2_0.ino
+++ Chron2-main/chron2_0/chron2_0.ino
@@ -1,20 +1,16 @@
-/*
-Created 2016
-by AlexGyver
-AlexGyver Home Labs Inc.
-*/
-
 #include <GyverOLED.h>
 GyverOLED<SSD1306_128x64, OLED_NO_BUFFER> oled;
 
-// int buttonPlusPin = 4;
-char masschar[5];  //массив символов для перевода
-int set,setmass,rapidtime;
-boolean initial,flagmass, flagmassset, rapidflag;  //флажки
-float dist=0.199;       //расстояние между датчиками в метрах  
-const int maxSize=4;
+#define btnPlusPin 4
+uint8_t btnPlusPrev;
+#define btnMinusPin 5
+uint8_t btnMinusPrev;
 
-int n=1;            //номер выстрела, начиная с 1
+int set;  // режим
+float dist=0.199;       //расстояние между датчиками в метрах  
+const int maxSize=4;  // количесво хранимых значений прошлых выстрелов
+
+int shotNum=1;            //номер выстрела, начиная с 1
 float velocity, energy;    //переменная для хранения скорости
 float BbMass=0.20;       //масса снаряда в килограммах
 volatile unsigned long gap1, gap2;    //отметки времени прохождения пулей датчиков
@@ -58,6 +54,7 @@ void UpdateSpeedValuesArr(float newVelocity) {
   SpeedValuesArr[0] =  newVelocity;
 }
 
+
 void SpeedRendererSetup() {
   oled.init();
   oled.clear();
@@ -97,6 +94,12 @@ void SpeedRendererSetup() {
   oled.setScale(1);
 }
 
+void massRenrererUpdater() {
+  oled.clear(0, 20, 24, 23);       // очистка прошлой энергии
+
+  oled.setCursor(0,2);
+  oled.print(String(BbMass));
+}
 
 void SpeedRenderer(float newVelocity, float newEnergy) {
   UpdateSpeedValuesArr(newVelocity);
@@ -109,7 +112,7 @@ void SpeedRenderer(float newVelocity, float newEnergy) {
   oled.setCursor(28,2);
   oled.print(String(newEnergy));
   oled.setCursor(60,2);
-  oled.print(String(n));
+  oled.print(String(shotNum));
 
   oled.setCursor(90,2);
   oled.print(String(speed02));
@@ -125,42 +128,36 @@ void SpeedRenderer(float newVelocity, float newEnergy) {
 
 
 void setup() {
+  pinMode(btnPlusPin, INPUT_PULLUP);
+  btnPlusPrev = digitalRead(btnPlusPin);
+  pinMode(btnMinusPin, INPUT_PULLUP);
+  btnMinusPrev = digitalRead(btnMinusPin);
+
   SpeedRendererSetup();
 
 	Serial.begin(9600);    //открываем COM порт
 	attachInterrupt(1,start,RISING);     //аппаратное прерывание при прохождении первого датчика
 	attachInterrupt(0,finish,RISING);      //аппаратное прерывание при прохождении второго датчика
 }
+
+
 void start() 
 {
 	if (gap1==0) {   //если измерение еще не проводилось
 		gap1=micros(); //получаем время работы ардуино с момента включения до момента пролетания первой пули
 	}
 }
+
+
 void finish() 
 {
 	if (gap2==0) {  //если измерение еще не проводилось
 		gap2=micros();  //получаем время работы ардуино с момента включения до момента пролетания второй пули
 	}
 }
+
+
 void loop() {
-	if (initial==0) {                          //флажок первого запуска
-		Serial.println("Press 1 to service mode");                      //режим отладки (резисторы)
-		Serial.println("Press 0 speed measure mode (default)");        //выход из режимов
-		Serial.println("System is ready, just pull the f*ckin trigger!");   //уведомление о том, что хрон готов к работе
-		Serial.println(" ");
-		initial=1;       //первый запуск, больше не показываем сообщения
-	}
-
-	if (Serial.available() > 0 && set!=2) {   //если есть какие буквы на вход с порта и не выбран 2 режим
-		int val=Serial.read();                  //прочитать что было послано в порт
-		switch(val) {                           //оператор выбора
-
-		case 48: set=0; flagmass=0; rapidflag=0; initial=0; break;    //если приняли 0 то выбрать 0 режим
-		case 49: set=1; break;                //если приняли 1 то запустить режим 1
-		}
-	}
-
 	if (set==1) {                    //если 1 режим
 		Serial.print("sensor 1: ");
 		Serial.println(analogRead(2));  //показать значение на первом датчике
@@ -168,14 +165,53 @@ void loop() {
 		Serial.print(analogRead(3));   //показать значение на втором датчике
 		Serial.println();
 		Serial.println();              //ну типо два переноса строки
+
+    oled.clear();
+    oled.setContrast(255);
+    oled.home();
+    oled.setScale(1);
+    oled.print("sensor 1:");
+    oled.setCursor(0,2);
+    oled.setScale(2);
+    oled.print(String(analogRead(2)));
+    oled.setScale(1);
+    oled.setCursor(0,4);
+    oled.print("sensor 2:");
+    oled.setScale(2);
+    oled.setCursor(0,6);
+    oled.print(String(analogRead(3)));
 	}
+
+  uint8_t btnPlus = digitalRead(btnPlusPin);
+  if (btnPlus == LOW && btnPlusPrev == HIGH && BbMass < 1)
+  {
+    BbMass = BbMass + 0.01;
+    massRenrererUpdater();
+  }
+  btnPlusPrev = digitalRead(btnPlusPin);
+
+  int8_t btnMinus = digitalRead(btnMinusPin);
+  if (btnMinus == LOW && btnMinusPrev == HIGH && BbMass > 0.1)
+  {
+    BbMass = BbMass - 0.01;
+    massRenrererUpdater();
+  }
+  btnMinusPrev = digitalRead(btnMinusPin);
+
+  if (btnPlus == LOW && btnMinus == LOW && set == 0) {
+    set = 1;
+  }
+  else if (btnPlus == LOW && btnMinus == LOW && set == 1) {
+    set = 0;
+    SpeedRendererSetup();
+  }
 
 
 	if (gap1!=0 && gap2!=0 && gap2>gap1 && set==0) {        //если пуля прошла оба датчика в 0 режиме
 		velocity=(1000000*(dist)/(gap2-gap1));         //вычисление скорости как расстояние/время
 		energy=velocity*velocity*BbMass*0.001/2;              //вычисление энергии
 		Serial.print("Shot #");                        
-		Serial.println(n);                                 //вывод номера выстрела
+		Serial.println(shotNum);                                 //вывод номера выстрела
 		Serial.print("Speed: ");    
 		Serial.println(velocity);                          //вывод скорости в COM
 		Serial.print("Energy: ");    
@@ -188,7 +224,7 @@ void loop() {
 
 		gap1=0;                                   //сброс значений
 		gap2=0;
-		n++;                                      //номер выстрела +1
+		shotNum++;                                      //номер выстрела +1
 	}
 
 	if (micros()-gap1>200000 && gap1!=0 && set!=1) { // (если пуля прошла первый датчик) И (прошла уже 0.2 секунды, а второй датчик не тронут)
@@ -199,5 +235,5 @@ void loop() {
 		gap2=0;
 	}
 
-	delay(200);    //задержка для стабильности
+	delay(100);    //задержка для стабильности
 }
